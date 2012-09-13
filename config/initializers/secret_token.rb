fa0a2ecb31e50bbc570af9a737568203e048d8a0
takehara <k.tginsei@gmail.com>
2012-09-13 09:30:42 +0900

    RoR環境構築

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyPractice::Application.config.secret_token = 'd00f45f2c1839b7864482d3896a16b2f991fe59f3b9de0605a14cd5583f7eca7ada217be4ee0549f63db7b1e68059af929213ac70330b6f353ba739a860574e0'
