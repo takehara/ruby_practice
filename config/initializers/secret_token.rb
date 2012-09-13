35d3a712ef62e369657f0c1ec4972607909aafdc
Take <k.tginsei@gmail.com>
2012-09-13 11:49:35 +0900

    mysqlで作り直し

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RubyPractice::Application.config.secret_token = 'd00f45f2c1839b7864482d3896a16b2f991fe59f3b9de0605a14cd5583f7eca7ada217be4ee0549f63db7b1e68059af929213ac70330b6f353ba739a860574e0'
+RubyPractice::Application.config.secret_token = 'd821ba838cdae78db5fcdaad3dbd05efb6210597f68f296a877b4785d1dc9e775a60ec555a666f3edcc904014871fd86307d92ea2e8a885b0032602b9b4676b2'
